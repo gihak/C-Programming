f9d60016ef31230d38024cf8c2a110c95eca8569
Gihak Kim <kgihak@gmail.com>
2025-09-08 21:48:16 +0900

    Commit

--- 2.c
+++ 2.c
@@ -4,14 +4,27 @@ int main(void) {
     printf("Hello, World!\nHello, World!\nHello, World!\nHello, World!\nHello, World!\nHello, World!\n");
     
     // 변수의 특징: 
-    // 데이터 타입 : 자료형
+    // 변수 타입 : 자료형
     // int :  10진수 정수형
     // float : 실수형
     // char : 문자형
-    int x = 10; // 변수 선언 및 초기화
-    int y = 20;
-    int sum = x + y;
-    printf("Sum: %d\n", sum);
+
+    int a; // 변수 타입 명시 -> 선언(정의)
+    a = 10; // 변수 초기화(값 할당)
+
+    int x = 20; // 선언과 초기화를 동시에
+    printf("x : %d\n", x); // 변수 사용. x는 그냥 값, 변수가 아님. = 이거를 기준으로 좌측에는 variable 우측에는 value
+
+    //변수는 중복 선언 금지
+    // int x = 30; // 선언과 초기화를 동시에
+    // printf("x : %d\n", x);
+
+    x = 30; // 변수의 재활용. 값 변경
+    printf("x : %d\n", x);
+
+    x = x + 10; // x의 기존 값에 10을 더한 후 다시 x에 저장
+    printf("x : %d\n", x);
+    
     return 0;
 
 }
