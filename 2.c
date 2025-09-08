452d6c4275814e1abaf03ac8408d4567acb06e0b
Gihak Kim <kgihak@gmail.com>
2025-09-08 21:27:41 +0900

    Add 2.c file with initial implementation

--- 2.c
+++ 2.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    printf("Hello, World!\nHello, World!\nHello, World!\nHello, World!\nHello, World!\nHello, World!\n");
+    
+    int x = 10;
+    int y = 20;
+    int sum = x + y;
+    printf("Sum: %d\n", sum);
     return 0;
+
 }
