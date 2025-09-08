983d5a59f78cd1998d67b607fa2c52d253bdd597
Gihak Kim <kgihak@gmail.com>
2025-09-08 21:31:58 +0900

    Commmit

--- 2.c
+++ 2.c
@@ -3,7 +3,12 @@
 int main(void) {
     printf("Hello, World!\nHello, World!\nHello, World!\nHello, World!\nHello, World!\nHello, World!\n");
     
-    int x = 10;
+    // 변수의 특징: 
+    // 데이터 타입 : 자료형
+    // int :  10진수 정수형
+    // float : 실수형
+    // char : 문자형
+    int x = 10; // 변수 선언 및 초기화
     int y = 20;
     int sum = x + y;
     printf("Sum: %d\n", sum);
