402fd0b8245de26c236a9fdb75776165f42829d1
Gihak Kim <kgihak@gmail.com>
2025-09-08 21:54:12 +0900

    Update 2.c

--- 2.c
+++ 2.c
@@ -8,7 +8,7 @@ int main(void) {
     // int :  10진수 정수형
     // float : 실수형
     // char : 문자형
-    
+
     int x = 20; // 선언과 초기화를 동시에
     printf("x : %d\n", x); // 변수 사용. x는 그냥 값, 변수가 아님. = 이거를 기준으로 좌측에는 variable 우측에는 value
 
@@ -21,7 +21,6 @@ int main(void) {
 
     x = x + 10; // x의 기존 값에 10을 더한 후 다시 x에 저장
     printf("x : %d\n", x);
-
     return 0;
 
 
